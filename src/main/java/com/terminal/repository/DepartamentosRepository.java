383d38bb7df24155db9ff3cc2640ffed65566749
Santiago Arias <santiari05@hotmail.com>
2025-04-25 23:54:55 -0500

    chore: Remove unused project files and configurations

--- src/main/java/com/terminal/repository/DepartamentosRepository.java
+++
@@ -1,7 +0,0 @@
-package com.terminal.repository;
-
-import com.terminal.entity.Departamentos;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DepartamentosRepository extends JpaRepository<Departamentos, Integer> {
-}
